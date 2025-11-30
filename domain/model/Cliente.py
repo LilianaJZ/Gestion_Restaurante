7fd579f5a44d4fa42b71b94c251f02650c612225
LilianaJZ <ljimenezz@unal.edu.co>
2025-11-30 18:07:28 -0500

    modificacion entidad cliente

--- domain/model/Cliente.py
+++ domain/model/Cliente.py
@@ -1,1 +1,50 @@
-#
+class Cliente:
+    def __init__(self, id=None, nombre=None, mail=None, telefono=None, ordenes=None):
+        self.__id = id
+        self.__nombre = nombre
+        self.__mail = mail
+        self.__telefono = telefono
+        self.__ordenes = ordenes if ordenes is not None else []  # agregación: un cliente tiene muchas órdenes
+
+    @property
+    def ordenes(self):
+        return self.__ordenes
+    @ordenes.setter
+    def ordenes(self, value):
+        self.__ordenes = value
+
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    @nombre.setter
+    def nombre(self, value):
+        self.__nombre = value
+
+    @property
+    def mail(self):
+        return self.__mail
+    @mail.setter
+    def mail(self, value):
+        self.__mail = value
+
+    @property
+    def telefono(self):
+        return self.__telefono
+    @telefono.setter
+    def telefono(self, value):
+        self.__telefono = value
+
+
+    def agregar_orden(self, orden):
+        """Agrega una orden al cliente (relación uno a muchos)"""
+        self.__ordenes.append(orden)
+
+    def __str__(self):
+        return f"Cliente(id={self.__id}, nombre={self.__nombre}, mail={self.__mail}, telefono={self.__telefono}, ordenes={len(self.__ordenes)})"
--- domain/service/Cliente_Service.py
+++ domain/service/Cliente_Service.py
@@ -1,1 +1,37 @@
-#
+from domain.model.Cliente import Cliente
+from persistence.Cliente_Repository import Cliente_Repository
+
+
+class Cliente_Service:
+    def __init__(self, cliente_repo: Cliente_Repository):
+        self.cliente_repo = cliente_repo
+
+    def crear_cliente(self, nombre, mail, telefono):
+        """Crea un nuevo cliente y lo guarda en la base de datos"""
+        cliente = Cliente(nombre=nombre, mail=mail, telefono=telefono)
+        self.cliente_repo.agregar_cliente(cliente)
+        return cliente
+
+    def listar_clientes(self):
+        """Devuelve todos los clientes"""
+        return self.cliente_repo.listar_clientes()
+
+    def obtener_cliente_por_id(self, cliente_id):
+        """Busca un cliente por su ID"""
+        return self.cliente_repo.obtener_cliente_por_id(cliente_id)
+
+    def actualizar_cliente(self, id, nombre, mail, telefono):
+        """Actualiza los datos de un cliente existente"""
+        cliente = Cliente(id=id, nombre=nombre, mail=mail, telefono=telefono)
+        self.cliente_repo.actualizar_cliente(cliente)
+        return cliente
+
+    def eliminar_cliente(self, cliente_id):
+        """Elimina un cliente por su ID"""
+        self.cliente_repo.eliminar_cliente(cliente_id)
+
+    def registrar_cliente(self, nombre, mail, telefono):
+        cliente = Cliente(nombre=nombre, mail=mail, telefono=telefono)
+        self.cliente_repo.agregar_cliente(cliente)
+        return cliente
+
